d5468cb7e6ac7e4d62376334fee2d7152ebf10c5
yongjo <yjlee90@suresofttech.com>
2022-08-30 09:59:12 +0900

    스프링 AOP 구현-스프링 AOP 구현2 - 포인트컷 분리

--- src/main/java/hello/aop/order/aop/AspectV2.java
+++ src/main/java/hello/aop/order/aop/AspectV2.java
@@ -4,13 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Slf4j
 @Aspect
-public class AspectV1 {
+public class AspectV2 {
 
     //hello.aop.order 패키지와 하위 패키지
-    @Around("execution(* hello.aop.order..*(..))")
+    @Pointcut("execution(* hello.aop.order..*(..))")
+    private void allOrder() {} //pointcut signature
+
+    @Around("allOrder()")
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
         log.info("[log] {}", joinPoint.getSignature()); //join point 시그니처
         return joinPoint.proceed();
