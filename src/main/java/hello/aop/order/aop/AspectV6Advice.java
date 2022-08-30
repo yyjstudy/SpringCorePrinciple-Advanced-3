d16d52ea2b30f43d06882e17dc04c97dc0c2a19d
yongjo <yjlee90@suresofttech.com>
2022-08-30 13:57:08 +0900

    스프링 AOP 구현-스프링 AOP 구현6 - 어드바이스 종류

---
+++ src/main/java/hello/aop/order/aop/AspectV6Advice.java
@@ -0,0 +1,63 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
+@Slf4j
+@Aspect
+public class AspectV6Advice {
+
+
+    @Around("hello.aop.order.aop.Pointcuts.orderAndService()")
+    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        try {
+            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+            Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
+            Parameter[] parameters = method.getParameters();
+            Object[] args = joinPoint.getArgs();
+            String[] parameterNames = signature.getParameterNames();
+
+            //@Before
+            log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
+            Object result = joinPoint.proceed();
+            //@AfterReturning
+            log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
+            return result;
+        } catch (Exception e) {
+            //@AfterThrowing
+            log.info("[트랜잭션 롤백] {}", joinPoint.getSignature());
+            throw e;
+        } finally {
+            //@After
+            log.info("[트랜잭션 리소스 릴리즈] {}", joinPoint.getSignature());
+        }
+    }
+
+    @Before("hello.aop.order.aop.Pointcuts.orderAndService()")
+    public void doBefore(JoinPoint joinPoint) {
+        log.info("[before] {}", joinPoint.getSignature());
+    }
+
+    @AfterReturning(value = "hello.aop.order.aop.Pointcuts.orderAndService()", returning = "result")
+    public void doReturn(JoinPoint joinPoint, Object result) {
+        log.info("[return] {} return={}", joinPoint.getSignature(), result);
+    }
+
+    @AfterThrowing(value = "hello.aop.order.aop.Pointcuts.orderAndService()", throwing = "ex")
+    public void doThrowing(JoinPoint joinPoint, Exception ex) {
+        log.info("[ex] {} message={}", ex);
+    }
+
+    @After(value = "hello.aop.order.aop.Pointcuts.orderAndService()")
+    public void doAfter(JoinPoint joinPoint) {
+        log.info("[after] {}", joinPoint.getSignature());
+    }
+}
